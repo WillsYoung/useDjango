9252cda7c3a9fb6228d7e31f0829d1e526df60e2
WillsYoung <38545505+WillsYoung@users.noreply.github.com>
2018-04-26 20:37:58 +0800

    Update urls.py
    工程项目的urls.py文件需要修改的地方

--- homework20180424/homework20180424/urls.py
+++ homework20180424/homework20180424/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # 这个scholl路径是我自己的app stu的相关事件处理的路径，即在url里面匹配到scholl即转到stuapp下面处理相应的请求
     url(r'school', include('stu.urls', namespace='s'))
 ]
 
 
 from stu.views import page_not_found, server_error
 
-
+# handler用来处理网页异常的反馈
 handler404 = page_not_found
 handler500 = server_error
